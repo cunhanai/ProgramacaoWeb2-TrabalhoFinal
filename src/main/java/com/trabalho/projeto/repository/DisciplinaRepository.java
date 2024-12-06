98435e48a4bdfa34e97bc6fa73efaf93d06de250
Ana Júlia da Cunha <101332630+cunhanai@users.noreply.github.com>
2024-12-05 23:58:36 -0300

    Removendo códigos não utilizados

--- src/main/java/com/trabalho/projeto/repository/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trabalho.projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.trabalho.projeto.model.Disciplina;
-
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Integer> {
-
-}
